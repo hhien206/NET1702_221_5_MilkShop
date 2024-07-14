53794c4a57121f14148c241c5b5c0a42c198767f
SE173396_LeNhatHuy <huylnse173396@fpt.edu.vn>
2024-07-15 00:51:30 +0700

    customerpage

--- MilkShopRazorWebApp/Pages/Customer.cshtml.cs
+++ MilkShopRazorWebApp/Pages/Customer.cshtml.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MilkShop.common;
-using MilkShopBusiness.Business;
 using MilkShopData.Models;
 using Tasks = System.Threading.Tasks;
 
